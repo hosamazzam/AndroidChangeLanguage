b2511df6eb202f6e9f7fa0e30ff75d80b5d480fb
hosamazzam <hosam_azzam2010@yahoo.com>
2017-03-08 22:41:03 +0200

    fix title change issues

--- app/src/main/java/com/freeapps/hosamazzam/androidchangelanguage/MainActivity.java
+++ app/src/main/java/com/freeapps/hosamazzam/androidchangelanguage/MainActivity.java
@@ -16,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getSupportActionBar().setTitle(R.string.app_name);
+
 
         findViewById(R.id.change).setOnClickListener(new View.OnClickListener() {
             @Override
