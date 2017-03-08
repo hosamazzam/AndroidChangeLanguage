94d21ad0d392a4d451ab0f79c33a9b0868298ea1
hosamazzam <hosam_azzam2010@yahoo.com>
2017-03-08 22:39:13 +0200

    release  project

--- app/src/main/java/com/freeapps/hosamazzam/androidchangelanguage/MainActivity.java
+++ app/src/main/java/com/freeapps/hosamazzam/androidchangelanguage/MainActivity.java
@@ -1,13 +1,50 @@
 package com.freeapps.hosamazzam.androidchangelanguage;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private String LANG_CURRENT = "en";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        findViewById(R.id.change).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (LANG_CURRENT.equals("en")) {
+                    changeLang(MainActivity.this, "ar");
+                } else {
+                    changeLang(MainActivity.this, "en");
+                }
+                finish();
+                startActivity(new Intent(MainActivity.this, MainActivity.class));
+            }
+        });
     }
+
+    public void changeLang(Context context, String lang) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("Language", lang);
+        editor.apply();
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(newBase);
+        LANG_CURRENT = preferences.getString("Language", "en");
+
+        super.attachBaseContext(MyContextWrapper.wrap(newBase, LANG_CURRENT));
+    }
+
 }
